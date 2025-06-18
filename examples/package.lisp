017d09fe916ebe52efe6fdb4f7445155e582b22b
Fabiano Pinto <fabiano.pinto@philips.com>
2025-06-18 20:35:36 +0200

    update examples export

--- examples/package.lisp
+++ examples/package.lisp
@@ -2,5 +2,22 @@
   (:use :cl :vorm)
   (:documentation "Examples of shape grammars for the VORM system")
   (:export :load-koch-curve
+           :load-koch-snowflake
            :create-koch-curve-grammar
-           :create-koch-snowflake-grammar))
+           :create-koch-snowflake-grammar
+           :load-sierpinski-triangle
+           :load-sierpinski-gasket
+           :create-sierpinski-triangle-grammar
+           :create-sierpinski-gasket-grammar
+           :load-mondrian-pattern
+           :load-recursive-grid
+           :load-architectural-pattern
+           :create-mondrian-grammar
+           :create-recursive-grid-grammar
+           :create-dynamic-architecture-grammar
+           :load-plant-structure
+           :load-fern-structure
+           :load-tree-structure
+           :create-plant-grammar
+           :create-fern-grammar
+           :create-tree-grammar))
